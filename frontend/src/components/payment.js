a1ea2de77a432b876876886830915bf87a26016d
Maleesha Dilshan <maleeshahirug@gmail.com>
2023-04-18 19:28:55 +0530

    image upload done

--- frontend/src/components/payment.js
+++ frontend/src/components/payment.js
@@ -1,10 +1,53 @@
-import React from 'react';
-//import {Link} from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
 
-export default function Payment(){
-    return(
+export default function Payment() {
+
+    const [image, setImages] = useState();
+
+    function onImageChange(e) {
+        setImages(e.target.files[0]);
+        console.log(image);
+    }
+
+
+    return (
         <div className='container'>
-            <h1>This is payment</h1>
+            <h1>Upload your slip here</h1>
+            <input type="file" accept="image/*" onChange={onImageChange} />
+
+            {image && (
+                <div style={styles.preview}>
+                    <img
+                        src={URL.createObjectURL(image)}
+                        style={styles.image}
+                        alt="Thumb"
+                    />
+                </div>
+            )}
+
         </div>
     );
 }
+
+const styles = {
+    container: {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        paddingTop: 50,
+    },
+    preview: {
+        marginTop: 50,
+        display: "flex",
+        flexDirection: "column",
+    },
+    image: { maxWidth: "100%", maxHeight: "100%" },
+    delete: {
+        cursor: "pointer",
+        padding: 15,
+        background: "red",
+        color: "white",
+        border: "none",
+    }
+};
